f33ef346453ff6c9c16dad2af388b057b4362bcd
Kyle <Kyle.Flavin@gmail.com>
2016-06-21 21:21:27 -0700

    Adjust setup.sh to handle rvm installation, and extend .bashrc to more easily customize.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
-# Setup dev environment
+sudo -v
+
+# setup bashrc
+if [ ! -f ~/.bashrc_mine ]; then
+    cp ./bashrc ~/.bashrc_mine
+fi
+
+grep -q 'source ~/.bashrc_mine' ~/.bashrc
+if [ $? -ne 0 ]; then
+    echo '# Source my configuration' >> ~/.bashrc
+    echo 'source ~/.bashrc_mine' >> ~/.bashrc
+fi
+
+function install_rvm {
+  sudo curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
+  sudo curl -sSL https://get.rvm.io | bash -s stable --ruby
+}
+
+
+# Setup python dev environment
 sudo pip install virtualenvwrapper
+
+# Ruby
+install_rvm
+
