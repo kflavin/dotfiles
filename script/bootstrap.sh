e12188e7c61719c58fdda10e286f043df2383689
Kyle <Kyle.Flavin@gmail.com>
2020-01-24 14:32:17 -0800

    Fix bin directory, and allow for "dot" files that are not actually hidden (like ~/bin/)

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -88,8 +88,15 @@ install_dotfiles() {
     info 'installing dotfiles'
     local overwrite_all=false backup_all=false skip_all=false
 
+    # symlink dot files (hidden)
     for src in $(find -H "$DOTFILES_ROOT" -maxdepth 2 -name '*.symlink' -not -path '*.git*'); do
         dst="$HOME/.$(basename "${src%.*}")"
+        link_file "$src" "$dst"
+    done
+
+    # symlink non-dot files (not hidden)
+    for src in $(find -H "$DOTFILES_ROOT" -maxdepth 2 -name '*.nodot_symlink' -not -path '*.git*'); do
+        dst="$HOME/$(basename "${src%.*}")"
         link_file "$src" "$dst"
     done
 
