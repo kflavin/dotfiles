fc37e9829dd5e083d759019174aa3e9a49c3d5ac
Kyle <Kyle.Flavin@gmail.com>
2020-05-15 19:41:08 -0700

    Update bin files

--- bin/bin.nodot_symlink/set_heroku_env.sh
+++ bin/bin.nodot_symlink/set_heroku_env.sh
@@ -2,10 +2,11 @@
 # read output from "heroku config -a rxvantage-api-test"
 
 $APP=$1
+$FILE=$2
 
 IFS=
 while read -r line; do
 key=$(echo $line | cut -f1 -d:)
 value=$(echo $line | rev | cut -d' ' -f1 | rev)
 heroku config:set -a ${APP} "$key"="$value"
-done <<< $(cat setvars)
+done <<< $(cat $FILE)
