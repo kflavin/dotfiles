a01c5bb9b25dd42ed12cd1f4469205a4a2b13a69
Kyle <Kyle.Flavin@gmail.com>
2020-11-21 13:21:56 -0800

    Add brew packages

--- macos/defaults/install.sh
+++ macos/defaults/install.sh
@@ -1,3 +1,28 @@
 # Set the default screenshot folder
 mkdir -p $HOME/screenshots
 defaults write com.app.screencapture location $HOME/screenshots
+
+brew install \
+  7zip \
+  TLSSLed \
+  ack \
+  aws-sam-cli \
+  certbot \
+  circleci \
+  cloc \
+  docker-machine-driver-xhyve \
+  fork \
+  git-delta \
+  git-gui \
+  gnupg \
+  gpg \
+  jsc \
+  jsonlint \
+  mapnik
+  boost-python3 \
+  meld \
+  pusher/brew/pusher
+  ripgrep \
+  sourcetree \
+  unix2dos \
+  yamllint
