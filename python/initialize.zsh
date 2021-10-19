ac6cd1e97e8a3fa7e64a0f3f9562a5025fa8653c
Kyle <Kyle.Flavin@gmail.com>
2021-10-19 11:24:41 -0700

    Fix pyenv

--- python/initialize.zsh
+++ python/initialize.zsh
@@ -1,5 +1,8 @@
+
 if command -v pyenv 1>/dev/null 2>&1; then
+  export PATH=$(pyenv root)/shims:$PATH
   eval "$(pyenv init -)"
+  #eval "$(pyenv init - | sed 's:^pyenv() :function pyenv():')"
 fi
 
 ## enable pyenv virtualenv
