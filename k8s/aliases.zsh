03546c6fc0cb2a5e39017d4f44e552cdf01e44a8
Kyle <Kyle.Flavin@gmail.com>
2021-10-26 18:08:12 -0700

    Add K8s aliases

--- k8s/aliases.zsh
+++ k8s/aliases.zsh
@@ -2,3 +2,6 @@ alias k="kubectl"
 alias kns="kubens"
 alias kctx="kubectx"
 alias ka="kubectl get pods -A"
+
+alias nodepod="kubectl get pod -o=custom-columns=NODE:.spec.nodeName,NAME:.metadata.name -A | sort"
+alias podnode="kubectl get pod -o=custom-columns=NAME:.metadata.name,STATUS:.status.phase,NODE:.spec.nodeName -A | sort"
