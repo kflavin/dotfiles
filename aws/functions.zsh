f74bd279b77b326fcfb1aeaac60436b1d90e6431
Kyle <Kyle.Flavin@gmail.com>
2022-02-03 18:24:10 -0800

    AWS functions for checking github runners

--- aws/functions.zsh
+++ aws/functions.zsh
@@ -3,3 +3,15 @@
 function publicip {
   aws ec2 describe-instances --query 'Reservations[*].Instances[].PublicIpAddress' --filter "Name=private-dns-name,Values=$1"
 }
+
+
+# These aren't very general, but I want to keep them in git.
+function ghrunners {
+  aws ec2 describe-instances --filter Name=tag:Name,Values=github-runner --query 'Reservations[].Instances[].NetworkInterfaces[].PrivateIpAddresses[?Primary==`true`].PrivateIpAddress[]' --no-cli-pager --output text
+}
+
+
+function checkrunners {
+    jump 'bash -c "for i in '$(ghrunners)'; do echo \$i; ssh -o StrictHostKeyChecking=false \$i which docker; done"'
+}
+
