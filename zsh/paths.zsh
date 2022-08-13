3ee690a757192cc8288ed96cc93a80b8dd8b407e
Kyle <Kyle.Flavin@gmail.com>
2022-08-13 12:57:57 -0700

    Add rancher to path

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -2,6 +2,10 @@
 mkdir -p ~/.bin_local
 export PATH="$PATH:$HOME/.bin_local:$HOME/bin"
 
+### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
+export PATH="/Users/kyleflavin/.rd/bin:$PATH"
+### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
+
 # jenv
 export PATH="$HOME/.jenv/bin:$PATH"
 eval "$(jenv init -)"
