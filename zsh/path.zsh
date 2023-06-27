1777c96413f4e52b82419369239c823016487924
Kyle <Kyle@rxvantage.com>
2023-06-27 14:08:50 -0700

    chore: update paths

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,9 +1,11 @@
+# Homebrew.  Put this first, but then allow shims to be prepended later on.
+# M1 is using /opt/homebrew
+export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
+eval "$(/usr/local/bin/brew shellenv)"
+
 #export PATH="$PATH:$HOME/.dotfiles/bin"
 mkdir -p ~/.bin_local
 export PATH="$PATH:$HOME/.bin_local:$HOME/bin"
-
-# Homebrew
-export PATH="$PATH:/opt/homebrew/bin"
 
 ### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
 export PATH="/Users/kyleflavin/.rd/bin:$PATH"
@@ -12,3 +14,4 @@ export PATH="/Users/kyleflavin/.rd/bin:$PATH"
 # jenv
 export PATH="$HOME/.jenv/bin:$PATH"
 eval "$(jenv init -)"
+
