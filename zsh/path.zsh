b6f634cc440d77c513f5412995a0f36be342116b
Kyle <Kyle.Flavin@gmail.com>
2023-08-08 09:55:39 -0700

    chore: path updates

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -17,7 +17,7 @@ mkdir -p ~/.bin_local
 export PATH="$PATH:$HOME/.bin_local:$HOME/bin"
 
 ### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
-export PATH="/Users/kyleflavin/.rd/bin:$PATH"
+export PATH="$HOME/.rd/bin:$PATH"
 ### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
 
 # jenv
