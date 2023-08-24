443d987861d374b8d429befd84e81eab0456edfc
Kyle <Kyle@rxvantage.com>
2023-08-24 15:30:45 -0700

    chore: update decode_jwt to ensure base64 strings are encoded properly

--- bin/bin.nodot_symlink/decode_jwt.sh
+++ bin/bin.nodot_symlink/decode_jwt.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-echo "$1" | awk -F. '{print $1}' | base64 -D
-echo "$1" | awk -F. '{print $2}' | base64 -D
-echo "$1" | awk -F. '{print $3}' | base64 -D
+echo "$1" | awk -F. '{printf $1}' | base64 -D
+echo
+
+length=$(echo -n "$1" | awk -F. '{printf $2}' | wc -m | tr -d ' ')
+payload=$(echo -n "$1" | awk -F. '{printf $2}')
+
+if [[ $(($length % 2)) -eq 1 ]]; then
+    payload="$payload="
+elif [[ $(($length % 4)) -eq 2 ]]; then
+    payload="$payload=="
+fi
+
+echo -n "$payload" | base64 -D | sed -E 's/(.*)/\1\n/' | jq 'with_entries(if .key == "created" or .key == "exp" then .value |= strftime("%Y-%m-%d %H:%M:%S") else . end)'
+echo
+
+# signature
+#echo "$1" | awk -F. '{printf $3}'
