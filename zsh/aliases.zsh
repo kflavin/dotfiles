226fd651a613f85a171e3101f78419138f1b1aa8
Kyle <Kyle@rxvantage.com>
2024-02-08 17:10:01 -0800

    chore: update vim and alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,4 @@
 alias chrome="open -a \"Google Chrome\""
 alias github_ssh_fingerprint="ssh-keygen -E md5 -lf"
 alias be="bundle exec"
-alias dc="docker compose --env-file .env --env-file .env.infra"
+alias dc="docker compose --env-file .env.infra"
