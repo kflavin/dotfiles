3243a30ac76830ec8b986d47b6591e89db697ddf
Kyle <Kyle@rxvantage.com>
2024-04-18 09:50:33 -0700

    add dive alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,3 +2,6 @@ alias chrome="open -a \"Google Chrome\""
 alias github_ssh_fingerprint="ssh-keygen -E md5 -lf"
 alias be="bundle exec"
 alias dc="docker compose --env-file .env.infra"
+
+# https://github.com/wagoodman/dive
+alias dive="docker run -ti --rm  -v /var/run/docker.sock:/var/run/docker.sock wagoodman/dive"
