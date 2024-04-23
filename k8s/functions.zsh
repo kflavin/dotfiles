d0074a4e67232a7ac8cb494e85e2dd3a47df9738
Kyle <Kyle@rxvantage.com>
2024-04-23 16:41:52 -0700

    run one off pod

--- k8s/functions.zsh
+++ k8s/functions.zsh
@@ -41,3 +41,37 @@ function helmuninstall() {
 function watchpods() {
     watch -n.1 -d "kubectl -n $1 get pods"
 }
+
+function krun() {
+    namespace=""
+
+    while getopts ":n:" opt; do
+      case ${opt} in
+        n )
+          namespace="$OPTARG"
+          ;;
+        \? )
+          echo "Invalid option: $OPTARG" 1>&2
+          ;;
+        : )
+          echo "Invalid option: $OPTARG requires an argument" 1>&2
+          ;;
+      esac
+    done
+    shift $((OPTIND -1))
+
+
+    image=${1:-alpine:latest}
+    cmd=${2:-sh}
+    #c=$(echo kubectl ${namespace}run -it --rm --restart=Never --image "$image" $(uuidgen | cut -d- -f1 | tr 'A-Z' 'a-z') -- "$cmd")
+    #kubectl -n "${namespace}" run -it --rm --restart=Never --image "$image" $(uuidgen | cut -d- -f1 | tr 'A-Z' 'a-z') -- "$cmd"
+    #bash -c "$c"
+
+    command=("kubectl" "run" "-it" "--rm" "--restart=Never" "--image" "$image")
+    if [[ -n "$namespace" ]]; then
+        command+=("-n" "$namespace")
+    fi
+    command+=("$(uuidgen | cut -d- -f1 | tr 'A-Z' 'a-z')" "--" "$cmd")
+    "${command[@]}"
+}
+
